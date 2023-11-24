19ec092e76e916f7b86de8dfc997f59463c95ccc
Gabriel Espósito <105060346+GabrielEsposito04@users.noreply.github.com>
2023-11-24 10:32:45 -0300

    Delete Abertura directory

--- Abertura/abertura.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _on_button_configuracao_pressed():
-	print('ir para configuração')
-	get_parent().mudar_cena(self, get_parent().get_node("Configuração"))
-
-
-
--- Abertura/principal.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func mudar_cena(atual:Node2D, nova:Node2D):
-	atual.visible = false
-	nova.visible = true
-	
-	
